b2a9794f6db52955be0ac6863530bc642c298ffd
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-05-24 22:17:59 +0700

    [Admin] - Create address module

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Filament\Resources\OrderResource\RelationManagers\AddressRelationManager;
+use App\Models\Address;
 use App\Models\Order;
 use App\Models\Product;
 use Filament\Forms;
@@ -241,7 +243,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            AddressRelationManager::class
         ];
     }
 
